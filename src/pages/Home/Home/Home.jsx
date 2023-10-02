0b341b733f40f6c962fe014bb5d42b89c70b0834
HasnaAkter <hasnaakter.cse@gmail.com>
2023-10-02 18:55:33 +0600

    menu-items

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Boxmodel from "../../Boxmodel/Boxmodel";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
+import PopularMenu from "../PopularMenu/PopularMenu";
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
            <Banner></Banner>
            <Category></Category>
            <Boxmodel></Boxmodel>
+           <PopularMenu></PopularMenu>
         </div>
     );
 };
---
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,15 @@
+import SectionTitle from "../../../components/SectionTitle"
+
+const PopularMenu = () => {
+    return (
+        <section>
+            <SectionTitle heading="Form Our Menu"
+            subHeading="Popular items">
+                
+            </SectionTitle>
+            
+        </section>
+    );
+};
+
+export default PopularMenu;
