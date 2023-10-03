bd9e6a583cad3d818f3089fd4a7c9cb931ebdb1c
HasnaAkter <hasnaakter.cse@gmail.com>
2023-10-03 20:52:15 +0600

    multiple cover set

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle"
 import { data } from "autoprefixer";
 import MenuItems from "../../../components/MenuItems";
-
+import { Link } from "react-router-dom";
 const PopularMenu = () => {
     const [menu,setMenu]=useState([])
     useEffect(() =>{
@@ -24,6 +24,8 @@ const PopularMenu = () => {
                     menu.map(item => <MenuItems key={item._id} item={item}></MenuItems>)
                 }
             </div>
+            <Link to="/menu"><div className="text-center">  <button className=" btn btn-outline border-0 border-b-4 mt-4">View All Menu</button></div> </Link>
+           
         </section>
     );
 };
--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,14 +1,25 @@
 import { Helmet } from "react-helmet";
-
+import Cover from "../../Home/Home/Shared/Cover/Cover";
+import menuimg from "../../../assets/menu/banner3.jpg";
+import PopularMenu from "../../Home/PopularMenu/PopularMenu"
 
 const Menu = () => {
-    return (
-        <div>
-            <Helmet><title>henaz | Menu</title></Helmet>
-            <h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed tenetur, non sunt iste iusto doloribus voluptatum amet accusamus? Dolor voluptatum repellat eius quis laboriosam iusto suscipit tempora obcaecati ad officia?</h2>
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>henaz | Menu</title>
+      </Helmet>
+
+      <Cover img={menuimg} title="our menu"></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={menuimg} title="our menu"></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={menuimg} title="our menu"></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={menuimg} title="our menu"></Cover>
+      <PopularMenu></PopularMenu>
+    </div>
+  );
 };
 
 export default Menu;
